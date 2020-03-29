157156e8afc48807e6e14d64b01cdf14f0c845d6
AndrewLi <13058100289@163.com>
2020-03-29 11:04:20 +0800

    create new branch

---
+++ service/dev.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        dev: "new branch"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
